d05d21eb7c3d9ca92c5ea8132ab53975c90f70ea
DEAN FREDERICK PURBA <deanfrederick690@gmail.com>
2026-04-15 21:02:03 +0700

    guided7 chapter 4-6 Next.js

---
+++ app/ui/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+<Image
+    src="/hero-desktop.png"
+    width={1000}
+    height={760}
+    className="hidden md:block"
+    alt="Screenshots of the dashboard project showing desktop version"
+/>
